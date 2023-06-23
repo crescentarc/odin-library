a9e6cd49e793b143e287512da698bbbbda980d8f
crescentarc <matthew.seth.taganile@gmail.com>
2023-06-23 11:36:34 -0400

    Add ability to display form by clicking 'Add Book'

---
+++ script.js
@@ -0,0 +1,14 @@
+let myBookLibrary = [];
+
+function Book() {
+}
+
+function addBookToLibrary() {
+
+}
+
+let newBookButton = document.querySelector("#new-book-button");
+newBookButton.addEventListener("click", function() {
+    let newBookForm = document.querySelector("#new-book-form");
+    newBookForm.style.display = "block";
+})
